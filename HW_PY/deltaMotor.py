32e842cc54140be32fd07860a015ee203791d925
liu zhenxiang <zhenxiang.liu@hcrobots.com>
2022-09-29 13:30:39 +0800

    HC_edition

--- HW_PY/deltaMotor.py
+++ HW_PY/deltaMotor.py
@@ -1,16 +1,74 @@
 import canopen
+import time
 
-def function1():
+def nodeConfig():
     network = canopen.Network()
     network.connect(bustype='slcan', channel='/dev/tty.usbmodem143301', bitrate=250000)
-    deltaMotorNode = network.add_node(2,'/Users/mac/tmp/HW_PY/ASDA-A3_v04.eds')
-    deltaMotorNode.sdo.download(0x2101, 0, b'\x0d\x01')
-    infile = deltaMotorNode.sdo.upload(0x2101, 0)
-    # Open a file for writing to
-    outfile = open('out.eds', 'w', encoding='ascii')
-    # Iteratively read lines from node and write to file
-    outfile.writelines(str(infile))
-    # Clean-up
+    deltaMotorNode = network.add_node(127,'/Users/mac/tmp/HW_PY/ASDA-A3_v04.eds')
+    deltaMotorNode.nmt.state = 'OPERATIONAL'
+    #docs https://hcrobots.feishu.cn/wiki/wikcnRos1XZ9nR1C5cXYaBgY1zd
+    #p1-01 value is 0x010C
+    deltaMotorNode.sdo[0x2101].write(0x010C)
+    #p1-42 value is 0
+    deltaMotorNode.sdo[0x212A].write(0x0)
+    #p1-43 value is 0
+    deltaMotorNode.sdo[0x212B].write(0x0)
+    #p2-10 value is 0124
+    deltaMotorNode.sdo[0x220A].write(0x0124)
+    #p2-11 value is 0
+    deltaMotorNode.sdo[0x220B].write(0x0)
+    #p2-12 value is 0
+    deltaMotorNode.sdo[0x220C].write(0x0)
+    #p2-13 value is 0
+    deltaMotorNode.sdo[0x220D].write(0x0)
+    #p2-14 value is 0
+    deltaMotorNode.sdo[0x220E].write(0x0)
+    #p2-15 value is 0
+    deltaMotorNode.sdo[0x220F].write(0x0)
+    #p2-16 value is 0
+    deltaMotorNode.sdo[0x2210].write(0x0)
+    #p2-17 value is 0021
+    deltaMotorNode.sdo[0x2211].write(0x0021)
+    #p2-18 value is 0
+    deltaMotorNode.sdo[0x2212].write(0x0)
+    #p2-19 value is 0
+    deltaMotorNode.sdo[0x2213].write(0x0)
+    #p2-20 value is 0108
+    deltaMotorNode.sdo[0x2214].write(0x0108)
+    #p2-21 value is 0
+    deltaMotorNode.sdo[0x2215].write(0x0)
+    #p2-22 value is 0
+    deltaMotorNode.sdo[0x2216].write(0x0)
+    #p3-00 value is 01(up)
+    deltaMotorNode.sdo[0x2300].write(0x01)
+    #p3-01 value is 0x0103 bitrate == 250000
+    deltaMotorNode.sdo[0x2301].write(0x0103)
+    print("node config finished!")
+
+def deltaMotorTest():
+    network = canopen.Network()
+    network.connect(bustype='slcan', channel='/dev/tty.usbmodem143201', bitrate=250000)
+    deltaMotorNode = network.add_node(1,'/Users/mac/tmp/HW_PY/ASDA-A3_v04.eds')
+    deltaMotorNode.nmt.state = 'OPERATIONAL'
+    deltaMotorNode.sdo[0x6040].write(0x00)
+    deltaMotorNode.sdo[0x6040].write(0x80)
+    deltaMotorNode.sdo[0x6040].write(0x00)
+    deltaMotorNode.sdo[0x6060].write(0x01)
+    deltaMotorNode.sdo[0x607A].write(-10000000)
+    deltaMotorNode.sdo[0x6081].write(500000)
+    deltaMotorNode.sdo[0x6040].write(0)
+    deltaMotorNode.sdo[0x6040].write(0x06)
+    deltaMotorNode.sdo[0x6040].write(0x07)
+    deltaMotorNode.sdo[0x6040].write(0x0f)
+    deltaMotorNode.sdo[0x6040].write(0x2f)
+    deltaMotorNode.sdo[0x6040].write(0x3f)
+    deltaMotorNode.sdo[0x6040].write(0x2f)
+    
+     
 
 if __name__ == "__main__":
-    function1()
+    # nodeConfig()
+    # print("please reboot your box during 10 seconds!")
+    # time.sleep(10)
+    deltaMotorTest()
+    
---
+++ HW_PY/findeNode.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+import canopen
+import time
+import sys
+
+network = canopen.Network()
+network.connect(bustype='slcan', channel='/dev/cu.usbmodem143201', bitrate=250000)
+max_node = 0x7F
+list = []
+def find_node():
+    node_index = 1
+    while (node_index < max_node):
+        node = network.add_node(node_index, 'canio.eds')
+        node.nmt.send_command(1)
+        device_type = node.sdo['Device Type']
+        try:
+            print('node:',node_index,',data is:',device_type.raw)
+        except:
+            #print(node_index,'is not find')
+            node_index = node_index + 1
+            continue
+        list.append(node_index)
+        node_index = node_index + 1
+    
+find_node()
+print('找到以下节点,十进制表示')
+print(list)
+
+
+
+
+
+
+
+
